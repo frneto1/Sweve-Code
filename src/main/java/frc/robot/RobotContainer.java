eeaf16a57012728d0f15b95fb1efa146cb73ed88
Francisco Neto <francisco.neto2903@gmail.com>
2026-01-14 14:58:04 -0300

    Update RobotContainer.java

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,20 +1,27 @@
 package frc.robot;
 
+import frc.robot.commands.SwerveAuto;
 import frc.robot.commands.SwerveLoc;
+import frc.robot.subsystems.Climber;
 import frc.robot.subsystems.SwerveWrapper;
-import edu.wpi.first.wpilibj.Joystick;
+import frc.robot.subsystems.Vision;
+import edu.wpi.first.wpilibj.PS5Controller;
 import edu.wpi.first.wpilibj2.command.Command;
 
 public class RobotContainer {
 
-  private Joystick bob = new Joystick(0);
+  private PS5Controller ps5 = new PS5Controller(1);
 
   public SwerveWrapper swerve = new SwerveWrapper();
+  public Climber climber = new Climber();
+  public Vision limelight = new Vision("null");  
+  public SwerveAuto swerveAuto = new SwerveAuto(limelight, swerve);
 
   public RobotContainer() {
-    swerve.setDefaultCommand(new SwerveLoc(swerve, bob));
+    swerve.setDefaultCommand(new SwerveLoc(swerve, ps5, swerveAuto, climber));
+    getAutonomousCommand();
   }
   public Command getAutonomousCommand() {
-    return null;
+    return swerveAuto;
     }
 }
