7b9edbf234d55a0f4dc19b1ee08d099e99d56306
Francisco Neto <francisco.neto2903@gmail.com>
2026-01-14 14:58:23 -0300

    Update SwerveWrapper.java

--- src/main/java/frc/robot/subsystems/SwerveWrapper.java
+++ src/main/java/frc/robot/subsystems/SwerveWrapper.java
@@ -3,24 +3,26 @@ package frc.robot.subsystems;
 import java.io.File;
 import java.io.IOException;
 
+
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.wpilibj.Filesystem;
-import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.PS5Controller;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
-import frc.robot.commands.SwerveLoc;
 import swervelib.SwerveDrive;
 import swervelib.parser.SwerveParser;
 
 public class SwerveWrapper extends SubsystemBase{
 
     private SwerveDrive swerve;
-    private Joystick bob = new Joystick(0);
-    double x = bob.getRawAxis(0);
-    double y = -bob.getRawAxis(1);
-    double rot = bob.getRawAxis(4);
+    private PS5Controller ps5 = new PS5Controller(1);
+    double x = ps5.getLeftX();
+    double y = -ps5.getLeftY();
+    double rot = ps5.getRightX();
+    public double speed;
 
     public SwerveWrapper(){
         File yagslDir = new File(Filesystem.getDeployDirectory(),"yagsl");
@@ -35,9 +37,10 @@ public class SwerveWrapper extends SubsystemBase{
     public void drive(double x, double y, double rot, boolean fieldRelative, boolean isOpenLoop){
         double vx = x * Constants.MAX_SPEED;
         double vy = y * Constants.MAX_SPEED;
+        
         double vrot = rot * Constants.MAX_ANGULAR_SPEED;
 
-        Translation2d vel = new Translation2d(vx, vy);
+        Translation2d vel = new Translation2d(vy, vx);
         swerve.drive(vel, vrot, fieldRelative, isOpenLoop);
     }
 
@@ -56,5 +59,8 @@ public class SwerveWrapper extends SubsystemBase{
     public void setModuleStates(SwerveModuleState[] states, boolean isOpenLoop) {
         swerve.setModuleStates(states, isOpenLoop);
     }
-
+    public void conversor(){
+        ChassisSpeeds velocities = swerve.getRobotVelocity();
+        speed = Math.sqrt(Math.pow(velocities.vxMetersPerSecond, 2) + Math.pow(velocities.vyMetersPerSecond, 2));
+    }
 }
