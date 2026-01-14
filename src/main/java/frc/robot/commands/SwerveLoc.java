a7c710f3f610564b9029a3c1c1bc0d66b94458fc
Francisco Neto <francisco.neto2903@gmail.com>
2026-01-14 14:56:31 -0300

    Update SwerveLoc.java

--- src/main/java/frc/robot/commands/SwerveLoc.java
+++ src/main/java/frc/robot/commands/SwerveLoc.java
@@ -1,28 +1,30 @@
 package frc.robot.commands;
 
-
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.wpilibj.PS5Controller;
 import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
-import frc.robot.Constants;
+import frc.robot.subsystems.Climber;
 import frc.robot.subsystems.SwerveWrapper;
 import swervelib.SwerveDrive;
 
 public class SwerveLoc extends Command {
 
+    private PS5Controller ps5 = new PS5Controller(1); 
     public SwerveWrapper swerveSubsystem;
+    public SwerveAuto swerveAuto;
     public SwerveDrive swerve;
-    public Joystick bob = new Joystick(0);
+    public Climber climber;
     double x, y, rot;
     double vx, vy, vrot;
 
-    public SwerveLoc(SwerveWrapper swerve, Joystick bob) {
+    public SwerveLoc(SwerveWrapper swerve, PS5Controller ps5, SwerveAuto swerveAuto, Climber climber) {
         this.swerveSubsystem = swerve;
-        this.bob = bob;
-        addRequirements(swerve);
+        this.ps5 = ps5;
+        this.swerveAuto = swerveAuto;
+        this.climber = climber;
+        addRequirements(swerveSubsystem);
     }
     public void initialize() {
         Pose2d poseInicial = new Pose2d(1.5, 5.5, Rotation2d.fromDegrees(180));
@@ -31,16 +33,19 @@ public class SwerveLoc extends Command {
     }
 
     public void execute() {
-        x = bob.getRawAxis(0);
-        y = -bob.getRawAxis(1);
-        rot = bob.getRawAxis(4);
 
-        x = Math.abs(x) < 0.04 ? 0 : bob.getRawAxis(0);
-        y = Math.abs(y) < 0.04 ? 0 : bob.getRawAxis(1); 
-        rot = Math.abs(rot) < 0.04 ? 0 : bob.getRawAxis(4);
+        x = -ps5.getLeftX();
+        y = -ps5.getLeftY();
+        rot = ps5.getRightX();
 
-        swerveSubsystem.drive(x, -y, rot, true, true);
-        dash();
+        x = Math.abs(x) < 0.05 ? 0 : ps5.getLeftX();
+        y = Math.abs(y) < 0.05 ? 0 : ps5.getLeftY();
+        rot = Math.abs(rot) < 0.05 ? 0 : ps5.getRightX();
+
+        swerveSubsystem.conversor();
+
+        swerveSubsystem.drive(x, -y, rot, false, true);
+        ControlClimber();
     }
 
     public void end(boolean interrupted) {
@@ -48,6 +53,24 @@ public class SwerveLoc extends Command {
     }
 
     public void dash(){
-        SmartDashboard.putBoolean("A", bob.getRawButton(1)); 
+
+        SmartDashboard.putNumber("Rot", rot);
+        SmartDashboard.putNumber("X", x);
+        SmartDashboard.putNumber("Y", y);
+        SmartDashboard.putNumber("% Velocidade", swerveSubsystem.speed * 33);
+        SmartDashboard.putNumber("m/s", swerveSubsystem.speed);
+
     }
+
+    public void ControlClimber(){
+    if (ps5.getPOV() == 0){
+        climber.motorClimb.set(1);
+    } 
+    else if (ps5.getPOV() == 180){
+        climber.motorClimb.set(-1);
+    }
+    else {
+        climber.motorClimb.set(0);
+    }
+}
 }
